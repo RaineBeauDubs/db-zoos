c26887189f1d487dec693f2f9bf7afffe007733f
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-26 16:41:55 -0500

    Get requests complete.

--- data/zoosRouter.js
+++ data/zoosRouter.js
@@ -5,10 +5,44 @@ const knex = require('knex');
 const knexConfig = {
   client: 'sqlite3',
   connection: {
-    filename: './data/lambda.db3'
+    filename: './data/lambda.sqlite3'
   },
   useNullAsDefault: true,
 };
+
+//GET REQUESTS
+
+router.get('/', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res
+        .status(200)
+        .json(zoos)
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json(error)
+    })
+});
+
+router.get('/:id', (req, res) => {
+  db('zoos')
+    .where({
+      id: req.params.id
+    })
+    .first()
+    .then(zoo => {
+      res
+        .status(200)
+        .json(zoo)
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json(error)
+    })
+});
 
 const db = knex(knexConfig);
 
